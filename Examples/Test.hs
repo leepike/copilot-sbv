5a1bfd99be1c11724560442edc7e82f4cdbd759e
Lee Pike <leepike@galois.com>
2011-07-17 21:37:15 -0700

    Updated instructions.

--- Examples/Test.hs
+++ Examples/Test.hs
@@ -27,4 +27,30 @@ spec = do
 main = do 
   reify spec >>= compile "test" 
 
+{-
+
+(0) Go into the generated XXX directory.
+
+(1) You need to post in something like the following in the generated
+    copilot_driver_XXX.c.  Basically, define the triggers and put in a main that
+    calls the main driver function.
+
+void trig1(SWord64 x) {
+  printf("trig1: %llu", x);
+}
+
+int main (void) {
+  int i=0;
+  for(;i<10;i++) {
+    driver_test();
+    printf("tmp_0: %i\n", tmp_0);
+  }
+  return 0;
+}
+
+(2) Then open the generated Makefile and change driver_sbv.c to copilot_driver_XXX.c
+
+(3) Type make.
+
+-}
 
